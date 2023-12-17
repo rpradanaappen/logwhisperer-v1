35763a91d439dfc0b8565653ba1fd2c9118cd005
rpradanaquadrant <rpradana@quadrant.io>
2023-12-17 20:39:59 +0700

    add functions

--- app.py
+++ app.py
@@ -22,12 +22,74 @@ password = ""
 
 result_buffer = []
 
+# ---- FUNCTIONS ----
 def check_elasticsearch_connection():
     try:
         es.ping()
         st.success("Connected to Elasticsearch! 🔗")
     except exceptions.ConnectionError:
         st.error("Unable to connect to Elasticsearch. Check your connection details.")
+
+def search_elasticsearch(log_group, start_date, end_date):
+    error_query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"range": {"@timestamp": {"gte": start_date, "lte": end_date}}},
+                    {"term": {"aws.cloudwatch.log_group": {"value": log_group}}},
+                    {"regexp": {"message": ".*error.*"}}
+                ]
+            }
+        }
+    }
+
+    result = es.search(index='logs-*', body=error_query)
+
+    hits = result['hits']['hits']
+    return hits
+
+def generate_summary(message):
+    try:
+        timestamp = message['_source'].get('@timestamp', 'N/A')
+        original_message = message['_source']['message'].strip()
+        max_context_length = 4096
+        truncated_message = original_message[:max_context_length]
+
+        summarizer = Summarizer()
+        summarized_message = summarizer(truncated_message)
+
+        prompt = (
+            f"analyze the following message:\n\n"
+            f"Original Message ({timestamp}): {truncated_message}\n\n"
+            "Your analysis should include 'Error Identification,' 'Root Cause Analysis,' and 'Resolutions.'"
+        )
+
+        result_buffer.append(prompt)
+
+    except Exception as e:
+        st.error(f"Error generating summary from OpenAI: {e}")
+
+
+def process_buffer():
+    if result_buffer:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt='\n\n'.join(result_buffer),
+            max_tokens=200,
+            temperature=0.5
+        )
+
+        if 'choices' in response and response['choices']:
+            if 'text' in response['choices'][0]:
+                summaries = response['choices'][0]['text'].split('\n\n')
+                for summary in summaries:
+                    st.text(f"Summarized Message: {summary}")
+            else:
+                st.error(f'Unexpected OpenAI response format')
+        else:
+            st.error(f'Unexpected OpenAI response format')
+
+        result_buffer.clear()
 
 # ---- PAGE CONFIG ----
 st.set_page_config(page_title="Log Whisperer", page_icon=":robot:", layout="wide")
@@ -65,8 +127,8 @@ with st.container():
     left_column, right_column = st.columns(2)
     with left_column:
         st.header("What is Log Whisperer?")
-        st.write("Tired of spending hours for searching root causes of your app errors? Here's some nifty solution to help you 👇")
         st.write("Log Whisperer will help you to analyze your log files to find errors in the code and give you recommendations on how to solve them")
+        st.write("Tired of spending hours for searching root causes of your app errors? Here's some nifty solution to help you 👇")
     with right_column:
         st_lottie(lottie_bug, height=300, key="coding")
 
@@ -76,11 +138,16 @@ with st.container():
     st.header("Let's begin analyzing your log file")
     st.write("##")
 
-    if "counter" not in st.session_state:
-        st.session_state.counter = 0
+    def analyze():
+        # check_elasticsearch_connection()
+        start_date_str = start_date.strftime("%Y-%m-%dT00:00:00.000Z")
+        end_date_str = (end_date + timedelta(days=1)).strftime("%Y-%m-%dT00:00:00.000Z")
+        # hits = search_elasticsearch(log_group, start_date_str, end_date_str)
 
-    def increment():
-        st.session_state.counter += 1
+        # for hit in hits:
+        #     generate_summary(hit)
+
+        # process_buffer()
 
     with st.form("analyze_form"):
             elasticsearch_index = st.text_input("Elasticsearch Index")
@@ -102,4 +169,4 @@ with st.container():
 
             st.write("")
 
-            submitted = st.form_submit_button("Analyze Logs", on_click=increment)
+            submitted = st.form_submit_button("Analyze Logs", on_click=analyze)
